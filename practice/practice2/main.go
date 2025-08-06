7b7ff617a527fa30c525e623c95dfecdeab9f503
wudianzheng <626968157@qq.com>
2025-08-06 23:25:01 +0800

    [add][回文]

--- practice/practice2/main.go
+++ practice/practice2/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+//回文
 
 func main() {
-	nums := []int{4, 1, 2, 1, 2}
-	fmt.Println(nums)
+	palindrome := isPalindrome(123)
+	palindrome1 := isPalindrome(121)
+	fmt.Println(palindrome, palindrome1)
+}
+
+func isPalindrome(x int) bool {
+	str := strconv.Itoa(x)
+	length := len(str)
+	for i := 0; i < length/2; i++ {
+		if str[i] != str[length-i-1] {
+			return false
+		}
+	}
+	return true
 }
